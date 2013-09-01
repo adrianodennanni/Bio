3467b70f353015bc158d9057f52b44db363eb1c5
adrianodennanni <adrianodennanni@gmail.com>
2013-09-01 14:09:01 -0300

    Code was indented and comments were added, only 1 PIC is now displayed, since the limit is 1.

--- Tests/Adriano/tipo1.rb
+++ Tests/Adriano/tipo1.rb
@@ -10,38 +10,47 @@ require "time"
 term1 = '#FaunaUSP'
 term2 = '#FloraUSP'
 
-  TweetStream.configure do |config|
-    config.consumer_key       = 'SFiHGCeVr6qMGx9pkxvBg'
-    config.consumer_secret    = 'bSu6UDz4TARxGHWqsvCmgJP8JnlJT31IKyaFBJNtcFY'
-    config.oauth_token        = '88300988-HMfsqoDKvp5LfjSYomtKMqtF0mx70P2Nf5wIjVC6f'
-    config.oauth_token_secret = 'qpbUxivfOyAwLBgYGF1p0R6F4K9WFVaPd2mpmscQgU'
-    config.auth_method        = :oauth
-  end
+TweetStream.configure do |config|
+	config.consumer_key       = 'SFiHGCeVr6qMGx9pkxvBg'
+	config.consumer_secret    = 'bSu6UDz4TARxGHWqsvCmgJP8JnlJT31IKyaFBJNtcFY'
+	config.oauth_token        = '88300988-HMfsqoDKvp5LfjSYomtKMqtF0mx70P2Nf5wIjVC6f'
+	config.oauth_token_secret = 'qpbUxivfOyAwLBgYGF1p0R6F4K9WFVaPd2mpmscQgU'
+	config.auth_method        = :oauth
+end
 
-puts "starting TweetStream.."
+puts "starting TweetStream..."
 TweetStream::Client.new.track(term1,term2,'#partiu','#gata','#braziliangirl','#vemnimim','#club','#night','#dormi','#swag','#sono','#comer','#tgif','#balada','#girl','#cama','#dance') do |status|
+	
+	#Username/exibition name
 	puts "Username: @#{status.user.screen_name}"
 	puts "Name: #{status.user.name}"
-
+	
+	#GeoTargeting
 	if status.geo!=nil
 		puts "Latitude: #{status.geo.coordinates[0]}"
 		puts "Longitude: #{status.geo.coordinates[1]}"
 	end
+	
+	#Picture uploaded with the tweet
+	while status.media[i]!=nil
+		puts "PIC: #{status.media[i].media_url}"
+	end
+	
+	#URLs pasted in the tweet
 	i = 0
-  while status.media[i]!=nil
-    puts "PIC #{i+1}: #{status.media[i].media_url}"
-    i = i + 1
-  end
-  i = 0
-  while status.urls[i]!=nil
-    puts "URL #{i+1}: #{status.urls[i].expanded_url}"
-    i = i + 1
-  end
-  i = 0
-  while status.hashtags[i]!=nil
-    puts "hashtag #{i+1}: #{status.hashtags[i].text}"
-    i = i + 1
-  end
+	while status.urls[i]!=nil
+		puts "URL #{i+1}: #{status.urls[i].expanded_url}"
+		i = i + 1
+	end
+	
+	#Hashtags used in the tweet
+	i = 0
+	while status.hashtags[i]!=nil
+		puts "hashtag ##{i+1}: #{status.hashtags[i].text}"
+		i = i + 1
+	end
+	
+	#Text/tweet ID
 	puts "Text: #{status.text}"
 	puts "ID: #{status.id}\n \n"
 
