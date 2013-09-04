4de3b1fe4eb0d94240118d5bba7842eebb6efdba
adrianodennanni <adrianodennanni@gmail.com>
2013-09-04 18:05:01 -0300

    Page layout is complete now.

--- Tests/Adriano/teste1/app/controllers/hello_controller.rb
+++ Tests/Adriano/teste1/app/controllers/hello_controller.rb
@@ -1,5 +1,17 @@
 class HelloController < ApplicationController
-
+	
+	@json = User.all.to_gmaps4rails do |user, marker|
+  marker.infowindow render_to_string(:partial => "/users/my_template", :locals => { :object => user})
+  marker.picture({
+                  :picture => "http://www.blankdots.com/img/github-32x32.png",
+                  :width   => 32,
+                  :height  => 32
+                 })
+  marker.title   "i'm the title"
+  marker.sidebar "i'm the sidebar"
+  marker.json({ :id => user.id, :foo => "bar" })
+end
+	
 	def world
 	
 	end
