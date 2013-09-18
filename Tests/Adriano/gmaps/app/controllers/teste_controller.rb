88bd16fd0598129d9b544b72131895b7aa2de0d4
adrianodennanni <adrianodennanni@gmail.com>
2013-09-18 00:47:27 -0300

    Page is very very good.

---
+++ Tests/Adriano/gmaps/app/controllers/teste_controller.rb
@@ -0,0 +1,2 @@
+class TesteController < ApplicationController
+end
---
+++ Tests/Adriano/gmaps/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmaps::Application.config.secret_token = '3cd23aa27f6b54f1db150cf4c3f008731427d91276579cfc0caf6fc8430dca5979c587587f904ad500f043c46433578dd1002535b9673524336b4ada1a1cd536'
