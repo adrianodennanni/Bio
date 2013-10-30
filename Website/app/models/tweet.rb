d32cac7aadb6febeb0aef01fb9571b763976046a
Kersul <joaohkfaria@gmail.com>
2013-10-30 00:23:39 -0200

    Updates on page design

--- Website/app/models/tweet.rb
+++ Website/app/models/tweet.rb
@@ -11,6 +11,6 @@ class Tweet < ActiveRecord::Base
       end
       def gmaps4rails_infowindow
         user = User.find_by_id_user(self.id_user)
-        "<div id='image'><a target='_blank' href='https://twitter.com/#{user.username}/status/#{self.id}'><img src='#{user.profile_image}' /></a></div>" << "<div id='name'><h1>#{user.name}</h1><h2>@#{user.username}</h2></div>" << "<br />" << "<div id='text'>#{self.text}</div>" << "<div id='picture'><img src='#{self.img_url}' width='290'></div>" << "<div id='date'><h2>#{self.date_tweet.strftime('%d/%m/%Y - %Hh%M')}</h2></div>" 
+        "<div id='image'><a target='_blank' href='https://twitter.com/#{user.username}'><img src='#{user.profile_image}' /></a></div>" << "<div id='name'><h1>#{user.name}</h1><h2>@#{user.username}</h2></div>" << "<br />" << "<div id='text'>#{self.text}</div>" << "<div id='picture'><a target='_blank' href='https://twitter.com/#{user.username}/status/#{self.id}'><img src='#{self.img_url}' width='290'></a></div>" << "<div id='date'><h2>#{self.date_tweet.strftime('%I:%M %p - %d/%m/%Y')}</h2></div>" 
       end
 end
