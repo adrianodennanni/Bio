b1f1c06ccfafb64bf12d1bcdbdb9a1f522ebcf52
Adriano Dennanni <adrianodennanni@gmail.com>
2014-07-12 08:05:43 -0300

    Like buttons were ajaxified; Corrections in pagination design

--- Website/app/controllers/map_controller.rb
+++ Website/app/controllers/map_controller.rb
@@ -2,9 +2,14 @@ class MapController < ApplicationController
 
   def index
     
-    @json = Tweet.all.where('latitude!=0 || longitude!=0')
     
-        
+    @q = Tweet.order('id_tweet DESC').where('latitude!=0 || longitude!=0').search(params[:q])
+    @latitude = Tweet.where("latitude != 0").last.latitude
+    @longitude = Tweet.where("longitude != 0").last.longitude
+    @tweets = @q.result(distinct: true).page(params[:page]).per(5)
+    
+    @json = @tweets
+            
     @hash = Gmaps4rails.build_markers(@json) do |json, marker|
       # marker recebe alguns parametros para cada marcador
       user=User.find(json.id_user)
@@ -12,12 +17,9 @@ class MapController < ApplicationController
       marker.lat json.latitude
       marker.lng json.longitude
       marker.infowindow infowindow_content
+      
     end
     
-    @q = Tweet.order('id_tweet DESC').where('latitude!=0 || longitude!=0').search(params[:q])
-    @latitude = Tweet.where("latitude != 0").last.latitude
-    @longitude = Tweet.where("longitude != 0").last.longitude
-    @tweets = @q.result(distinct: true).page(params[:page]).per(5)
   end
   
 end
